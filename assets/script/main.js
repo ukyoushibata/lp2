63973584ab19c6a856852d3c1a3af612ef05b558
ukyoushibata <xfnr9710@yahoo.co.jp>
2024-08-03 21:35:07 +0900

    8月3日

---
+++ assets/script/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.a-slider').slick({
+    autoplay: true,
+    arrows: true,
+    dots: true,
+    prevArrow: '<img src="/assets/image/arrow-left-circle-fill.jpg" class="slide-arrow prev-arrow">',
+    nextArrow: '<img src="/assets/image/arrow-right-circle-fill.jpg" class="slide-arrow next-arrow">',
+    // その他のオプション
+    });
+    });
+
+    $('.question__accordion__header:nth-child(1)').addClass('active');
+    $('.question__accordion__content:nth-child(2)').css('display', 'block');
+    $('.question__accordion__header').click(function() {
+    $(this).next().slideToggle();
+    $(this).toggleClass('active');
+    });
