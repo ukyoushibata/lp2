e218e6bda3948fa7c98e765928897c6af9678227
ukyoushibata <xfnr9710@yahoo.co.jp>
2024-08-04 18:15:12 +0900

    8月4日18時

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
-    $('.a-slider').slick({
+    $('.voices__slider').slick({
     autoplay: true,
     arrows: true,
     dots: true,
-    prevArrow: '<img src="/assets/image/arrow-left-circle-fill.jpg" class="slide-arrow prev-arrow">',
-    nextArrow: '<img src="/assets/image/arrow-right-circle-fill.jpg" class="slide-arrow next-arrow">',
-    // その他のオプション
+    prevArrow: '<div><img src="/assets/image/arrow-left-circle-fill.jpg" class="slide-arrow prev-arrow"></div>',
+    nextArrow: '<div><img src="/assets/image/arrow-right-circle-fill.jpg" class="slide-arrow next-arrow"></div>',
     });
     });
 
