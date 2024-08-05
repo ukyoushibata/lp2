088fad12df265bc946495cd18fd1f6be8949ec54
ukyoushibata <xfnr9710@yahoo.co.jp>
2024-08-05 23:17:26 +0900

    8月5日23時

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,16 +1,26 @@
 $(document).ready(function(){
     $('.voices__slider').slick({
-    autoplay: true,
-    arrows: true,
-    dots: true,
-    prevArrow: '<div><img src="/assets/image/arrow-left-circle-fill.jpg" class="slide-arrow prev-arrow"></div>',
-    nextArrow: '<div><img src="/assets/image/arrow-right-circle-fill.jpg" class="slide-arrow next-arrow"></div>',
+        autoplay: false,
+        arrows: true,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: '<div><img src="/assets/image/arrow-left-circle-fill.jpg" class="slide-arrow prev-arrow"></div>',
+        nextArrow: '<div><img src="/assets/image/arrow-right-circle-fill.jpg" class="slide-arrow next-arrow"></div>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
     });
-    });
+});
 
-    $('.question__accordion__header:nth-child(1)').addClass('active');
-    $('.question__accordion__content:nth-child(2)').css('display', 'block');
-    $('.question__accordion__header').click(function() {
+// $('.question__accordion__header:nth-child(1)').addClass('active');
+// $('.question__accordion__content:nth-child(2)').css('display', 'block');
+$('.question__accordion__header').click(function() {
     $(this).next().slideToggle();
     $(this).toggleClass('active');
-    });
+});
