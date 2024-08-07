a2b96787300721cf1638543f265b8b21ab19dca8
ukyoushibata <xfnr9710@yahoo.co.jp>
2024-08-07 22:11:48 +0900

    LP2初稿提出

--- assets/script/main.js
+++ assets/script/main.js
@@ -5,22 +5,20 @@ $(document).ready(function(){
         dots: true,
         slidesToShow: 3,
         slidesToScroll: 3,
-        prevArrow: '<div><img src="/assets/image/arrow-left-circle-fill.jpg" class="slide-arrow prev-arrow"></div>',
-        nextArrow: '<div><img src="/assets/image/arrow-right-circle-fill.jpg" class="slide-arrow next-arrow"></div>',
+        prevArrow: '<img src="/assets/image/arrow-left-circle-fill.jpg" class="slide-arrow prev-arrow">',
+        nextArrow: '<img src="/assets/image/arrow-right-circle-fill.jpg" class="slide-arrow next-arrow">',
         responsive: [
             {
                 breakpoint: 768,
                 settings: {
                     slidesToShow: 1,
+                    slidesToScroll: 1,
                 }
             }
         ]
     });
 });
 
-// $('.question__accordion__header:nth-child(1)').addClass('active');
-// $('.question__accordion__content:nth-child(2)').css('display', 'block');
 $('.question__accordion__header').click(function() {
     $(this).next().slideToggle();
-    $(this).toggleClass('active');
 });
