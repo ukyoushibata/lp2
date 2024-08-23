b44a790b9fe067d26ba6e86c8ade8e1bb62179e4
ukyou.shibata <xfnr9710@yahoo.co.jp>
2024-08-23 21:37:01 +0900

    8月23日（金）

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.voices__slider').slick({
         autoplay: false,
         arrows: true,
         dots: true,
         slidesToShow: 3,
         slidesToScroll: 3,
-        prevArrow: '<img src="/assets/image/arrow-left-circle-fill.jpg" class="slide-arrow prev-arrow">',
-        nextArrow: '<img src="/assets/image/arrow-right-circle-fill.jpg" class="slide-arrow next-arrow">',
+        prevArrow: '<img src="/assets/image/arrow_left_circle_fill.jpg" class="slide-arrow prev-arrow">',
+        nextArrow: '<img src="/assets/image/arrow_right_circle_fill.jpg" class="slide-arrow next-arrow">',
         responsive: [
             {
                 breakpoint: 768,
@@ -19,6 +19,6 @@ $(document).ready(function(){
     });
 });
 
-$('.question__accordion__header').click(function() {
+$('.question__accordion__header').click(function () {
     $(this).next().slideToggle();
 });
